1ad974f3553eaa546276877eab8b5ef8594c9d8c
mquesada02 <mquesada@itba.edu.ar>
2023-09-30 14:19:59 -0300

    Small changes on login

--- fiit-web/src/App.vue
+++ fiit-web/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <Register/>
+      <InicioSesion />
     </v-main>
   </v-app>
 </template>
--- fiit-web/src/components/LogIn.vue
+++ fiit-web/src/components/LogIn.vue
@@ -1,9 +1,10 @@
 <template>
-  <v-sheet width="300" class="mx-auto">
+  <v-sheet width="300" class="mx-auto" rounded color="white">
     <v-form fast-fail @submit.prevent>
-    <p class="text-h3 text-center drop-shadow-lg my-10" >
-             Iniciar Sesion
-        </p>
+    <p class="pt-4 font-weight-bold text-h4 text-center drop-shadow-lg mb-4" >
+             Iniciar Sesión
+    </p>
+    <v-container>
       <v-text-field
         v-model="firstName"
         label="Usuario/E-Mail"
@@ -16,9 +17,11 @@
         :rules="contraseñaRules"
      ></v-text-field>
     <p class="drop-shadow-lg  text-center my-4">
-             ¿No tenes cuenta? <v-btn type="submit">Registrate </v-btn> 
+             ¿No tenes cuenta? 
+             <v-btn type="submit" color="secondary">Registrate</v-btn> 
     </p>
     <v-btn variant="outlined" type="submit" block class="text-center"> Iniciar Sesión </v-btn>
+    </v-container>
     </v-form>
   </v-sheet>
 </template>
