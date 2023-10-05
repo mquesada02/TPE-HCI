a3d109829aa34b1faa92c6e3f5aea01f5be0a859
Thomas Ruijgt <truijgt@itba.edu.ar>
2023-10-05 16:48:26 -0300

    avances en Favs

---
+++ fiit-web/src/components/MyFavs.vue
@@ -0,0 +1,55 @@
+<template>
+    <p class="pl-3 pt-10 text-h4 font-weight-medium">
+        Mis favoritos:
+    </p>
+    <div v-for="product in items">
+      <p>{{product.title}}</p>
+    </div>
+    <div class="text-center">
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="4">
+          <v-container class="max-width">
+            <v-pagination
+            v-model="paginationData.currentPage"
+            :length="paginationData.totalPages"
+            @input="changePage"
+            ></v-pagination>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+    <div class="text-center">
+        
+    </div>
+</template>
+
+<script>
+const props = defineProps(['items'])
+const items = props.items
+export default {
+  data() {
+    return {
+      paginationData: {
+        currentPage: 1, //no tiene q decir un numero, tiene decir la q se ese usando
+        itemsPerPage: 2, 
+        totalPages: 15, //en realidad el numero total de páginas debe ser medido por cuantas rutinas tiene la persona, osea la api
+      },
+    };
+  },
+  computed: {
+    displayedItems() {
+      const startIndex = (this.paginationData.currentPage - 1) * this.paginationData.itemsPerPage;
+      const endIndex = startIndex + this.paginationData.itemsPerPage;
+      return this.items.slice(startIndex, endIndex);
+    },
+  },
+  methods: {
+    changePage(page) {
+      this.paginationData.currentPage = page;
+      //tiene q cambiar cuando hay un click
+    },
+  },
+};
+</script>
--- fiit-web/src/views/Favorites.vue
+++ fiit-web/src/views/Favorites.vue
@@ -3,6 +3,7 @@
     <v-main color="background">
       <v-container>
         <FavsRec title="Recientes" :imgs="recientes"/>
+        <Myfavs items="items"/>
       </v-container>
     </v-main>
   </v-app>
@@ -10,6 +11,7 @@
 
 <script setup>
     import FavsRec from '@/components/FavsRec.vue';
+    import Myfavs from '@/components/MyFavs.vue';
     const recientes = [
   { src: 'abdominales bicicleta.webp', title: 'Abdominales en bicicleta' },
     { src: 'abductores.jpg', title: 'Abductores' },
@@ -17,5 +19,12 @@
     { src: 'elevaciones laterales.jpeg', title: 'Elevaciones laterales' },
     { src: 'estiramiento abductores.jpg', title: 'Estiramiento de abductores' }
   ];
+   const items = [  //copie lo mismo q en recientes, pero deberia ir lo q diga la api 
+   { src: 'abdominales bicicleta.webp', title: 'Abdominales en bicicleta' },
+    { src: 'abductores.jpg', title: 'Abductores' },
+    { src: 'dominadas.jpg', title: 'Dominadas' },
+    { src: 'elevaciones laterales.jpeg', title: 'Elevaciones laterales' },
+    { src: 'estiramiento abductores.jpg', title: 'Estiramiento de abductores' }
+   ];
 </script>
 
