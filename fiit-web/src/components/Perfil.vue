9efc9209c3fc196f5d92450a8813d99dc4f0a886
Florencia222 <fcarrica@itba.edu.ar>
2023-10-08 11:54:19 -0300

    Profile

---
+++ fiit-web/src/components/Perfil.vue
@@ -0,0 +1,45 @@
+<template>
+<v-container>
+
+  <v-sheet  width="500" height="700" class="mx-auto mb-4" color="secondary">
+   <v-row class="mb-6" align="center" justify="center"></v-row>
+  <v-sheet  width="450" height="650" class="mx-auto mt-4"  color="lighter" rounded="lg" style="border: 1px solid black;" >
+  <v-img src="@/assets/img/lapiz.png" class="mx-auto mt-4 mr-4" align="left"
+          max-height="20"
+          max-width="20"></v-img>
+  <v-row class="mt-4" align="center" justify="center">
+      <v-col cols="12" sm="6" md="4">
+        <v-img
+          src="@/assets/img/perfil.png"
+          class="mx-auto mt-4 mb-4"
+          max-height="200"
+          max-width="200"
+          
+        ></v-img>
+        </v-col>
+    </v-row>
+        <h1 class="mb-4 subrayado" align="center"> <b> Olivia Navarro</b></h1>
+        <p class=" ml-12 mr-12 mb-4 myParagraph"> Me considero una persona apasionada por el fitness. No dudes en escribirme cualquier consulta!</p>
+        <p  align="center" class="myParagraph"> Edad:42</p>
+        <p  align="center" class="myParagraph"> Peso:70</p>
+        <div class="d-flex flex-column align-center mb-4">
+         <v-btn color="secondary" type="submit" class="text-center mt-8">Cerrar Sesion</v-btn>
+         </div>
+      
+   
+  </v-sheet>
+
+  </v-sheet>
+  </v-container>
+</template>
+<style>
+.myParagraph {
+  font-size: 27px;
+}
+ .subrayado {
+      text-decoration: underline;
+    }
+  
+
+
+</style>
---
+++ fiit-web/src/views/Profile.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <v-container fill-height>
+      <v-row align="center" justify="center">
+        <v-col cols="4">
+        <container >
+          <Perfil />
+          </container>
+        </v-col>
+        <v-col cols="2">
+        </v-col>
+        <v-col cols="6">
+        <container >
+           <SmallCarousel title="Mis rutinas:" :imgs="destacadas"/>
+        <SmallCarousel title="Favoritas:" :imgs="recientes"/>
+        </container>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script setup>
+
+    import Perfil from '@/components/Perfil.vue';
+    import FullCarousel from '@/components/FullCarousel.vue';
+  import SmallCarousel from '@/components/SmallCarousel.vue';
+
+ const destacadas = [
+    { src: 'abdominales bicicleta.webp', title: 'Abdominales en bicicleta' },
+    { src: 'abductores.jpg', title: 'Abductores' },
+    { src: 'dominadas.jpg', title: 'Dominadas' },
+    { src: 'elevaciones laterales.jpeg', title: 'Elevaciones laterales' },
+    { src: 'estiramiento abductores.jpg', title: 'Estiramiento de abductores' },
+    { src: 'fondo triceps.webp', title: 'Fondo de tríceps' },
+    { src: 'peso muerto.jpg', title: 'Peso muerto' },
+    { src: 'sentadillas.jpg', title: 'Sentadillas' },
+    { src: 'zancadas.webp', title: 'Zancadas' }
+  ];
+  const recientes = [
+  { src: 'abdominales bicicleta.webp', title: 'Abdominales en bicicleta' },
+    { src: 'abductores.jpg', title: 'Abductores' },
+    { src: 'dominadas.jpg', title: 'Dominadas' },
+    { src: 'elevaciones laterales.jpeg', title: 'Elevaciones laterales' },
+    { src: 'estiramiento abductores.jpg', title: 'Estiramiento de abductores' },
+    { src: 'fondo triceps.webp', title: 'Fondo de tríceps' },
+    { src: 'peso muerto.jpg', title: 'Peso muerto' },
+    { src: 'sentadillas.jpg', title: 'Sentadillas' },
+    { src: 'zancadas.webp', title: 'Zancadas' }
+  ];
+  </script>
