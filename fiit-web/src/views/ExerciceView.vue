2e7d044ba75ddcd2b4ad52e0a264033188f5bd98
Florencia222 <fcarrica@itba.edu.ar>
2023-10-12 13:05:19 -0300

    ExerciceView

---
+++ fiit-web/src/views/ExerciceView.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main color="background">
+      <v-container>
+      <Exercice />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+    import Exercice from '@/components/Exercice.vue';
+    </script>
