d57b630fccf2d5591b522aafc880da4d6ce8c95a
Florencia222 <fcarrica@itba.edu.ar>
2023-10-20 13:12:06 -0300

    CreateExercice

---
+++ fiit-web/src/views/CreateExerciceView.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-main color="background">
+      <v-container>
+      <CreateExcercice />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+    import CreateExcercice from '@/components/CreateExcercice.vue';
+    </script>
