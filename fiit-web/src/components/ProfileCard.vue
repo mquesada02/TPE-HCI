ed5ba9b19a70b82ca8a4c2048c0830bf4241e0c9
mquesada02 <mquesada@itba.edu.ar>
2023-10-22 13:17:47 -0300

    Perfil de mis rutinas

--- fiit-web/src/components/ProfileCard.vue
+++ fiit-web/src/components/ProfileCard.vue
@@ -1,36 +1,34 @@
 <template>
-    <v-container class="d-flex justify-center pa-10">
-        <v-sheet height="100%" width="40%" color="secondary" rounded="xl" elevation="10" class="d-flex justify-center pa-5">
-            <v-sheet v-if="!modifyMode" height="100%" width="100%" color="lighter" rounded="xl" class="pa-0 text-center">
-                <div class="pa-4 d-flex flex-row-reverse">
-                    <v-btn @click="modify()" size="50" color="primary" icon="mdi-pencil" />
-                </div>
-                <v-avatar color="background" size="150">
-                    <v-icon v-if="!avatar" size="150" class="mdi" icon="mdi-account" />
-                    <v-img v-else :src="avatar" class="white--text" height="150" width="150" />
-                </v-avatar>
-                <p class="pt-4 font-weight-bold text-h2 text-center drop-shadow-lg mb-4">{{ user.username }}</p>
-                <p class="text-h5 text-center drop-shadow-lg mb-4">{{ firstName }} {{ lastName }}</p>
-                <p class="text-h6 text-center drop-shadow-lg mb-4">Edad: {{ age }} años</p>
-                <p class="text-h6 text-center drop-shadow-lg mb-4">Peso: {{ weight }}kg</p>
-                <p class="text-h6 text-center drop-shadow-lg mb-4">Altura: {{ height }}cm</p>
-                <v-btn color="secondary" type="submit" @click="logOut" class="text-center mb-8">Cerrar Sesion</v-btn>
-            </v-sheet>
-            <v-sheet v-else height="100%" width="100%" color="lighter" rounded="xl" class="pa-0 text-center">
-                <v-form @submit.prevent="onSubmit">
-                    <v-text-field type="text" v-model:model-value="avatar" label="Avatar URL"></v-text-field>
-                    <img v-if="avatar" :src="avatar" height="150" width="150" />
-                    <v-text-field type="text" v-model:model-value="firstName" label="Nombre"></v-text-field>
-                    <v-text-field type="text" v-model:model-value="lastName" label="Apellido"></v-text-field>
-                    <v-text-field type="text" v-model:model-value="age" label="Edad"></v-text-field>
-                    <v-text-field type="text" v-model:model-value="weight" label="Peso"></v-text-field>
-                    <v-text-field type="text" v-model:model-value="height" label="Altura"></v-text-field>
-                    <v-btn variant="outlined" :loading="loading" type="submit" block class="text-center"> Guardar cambios </v-btn>
-                </v-form>
-            </v-sheet>
+    <v-sheet height="100%" width="30vw" color="secondary" rounded="xl" elevation="10" class="d-flex justify-center ma-5">
+        <v-sheet v-if="!modifyMode" height="95%" width="95%" color="lighter" elevation="10" rounded="xl" class=" ma-3 text-center">
+            <div class="pa-4 d-flex flex-row-reverse">
+                <v-btn @click="modify()" size="50" color="primary" icon="mdi-pencil" />
+            </div>
+            <v-avatar color="background" size="150">
+                <v-icon v-if="!avatar" size="150" class="mdi" icon="mdi-account" />
+                <v-img v-else :src="avatar" class="white--text" height="150" width="150" />
+            </v-avatar>
+            <p class="pt-4 font-weight-bold text-h2 text-center drop-shadow-lg mb-4">{{ user.username }}</p>
+            <p class="text-h5 text-center drop-shadow-lg mb-4">{{ firstName }} {{ lastName }}</p>
+            <p class="text-h6 text-center drop-shadow-lg mb-4">Edad: {{ age }} años</p>
+            <p class="text-h6 text-center drop-shadow-lg mb-4">Peso: {{ weight }}kg</p>
+            <p class="text-h6 text-center drop-shadow-lg mb-4">Altura: {{ height }}cm</p>
+            <v-btn color="secondary" type="submit" @click="logOut" class="text-center mb-8">Cerrar Sesion</v-btn>
         </v-sheet>
-        <AlertSnackbar />
-    </v-container>
+        <v-sheet v-else height="100%" width="100%" color="lighter" rounded="xl" class="pa-0 text-center">
+            <v-form @submit.prevent="onSubmit">
+                <v-text-field type="text" v-model:model-value="avatar" label="Avatar URL"></v-text-field>
+                <img v-if="avatar" :src="avatar" height="150" width="150" />
+                <v-text-field type="text" v-model:model-value="firstName" label="Nombre"></v-text-field>
+                <v-text-field type="text" v-model:model-value="lastName" label="Apellido"></v-text-field>
+                <v-text-field type="text" v-model:model-value="age" label="Edad"></v-text-field>
+                <v-text-field type="text" v-model:model-value="weight" label="Peso"></v-text-field>
+                <v-text-field type="text" v-model:model-value="height" label="Altura"></v-text-field>
+                <v-btn variant="outlined" :loading="loading" type="submit" block class="text-center"> Guardar cambios
+                </v-btn>
+            </v-form>
+        </v-sheet>
+    </v-sheet>
 </template>
 
 <script setup>
@@ -75,20 +73,20 @@ function modify() {
     modifyMode.value = true;
 }
 
-async function logOut () {
+async function logOut() {
     try {
-      await userStore.logout();
-      
+        await userStore.logout();
+
     } catch (error) {
-      
+
     } finally {
         router.push('/');
     }
-  };
+};
 
 async function onSubmit() {
     loading.value = true;
-    const newBirthdate = new Date('01-01-' + (new Date().getFullYear() - age.value)+1);
+    const newBirthdate = new Date('01-01-' + (new Date().getFullYear() - age.value) + 1);
     const userInfo = new UserInfoWithAvatar(
         firstName.value ? firstName.value : user.value.firstName,
         lastName.value ? lastName.value : user.value.lastName,
@@ -97,10 +95,10 @@ async function onSubmit() {
         height.value ? height.value : user.value.metadata.height,
         weight.value ? weight.value : user.value.metadata.weight,
         avatar.value ? avatar.value : user.value.avatarUrl,
-        );
+    );
     try {
         await userStore.modifyCurrentUser(userInfo);
-        
+
     } catch (error) {
         text.value = error.description;
         snackbar.value = true;
@@ -108,8 +106,8 @@ async function onSubmit() {
         modifyMode.value = false;
         loading.value = false;
     }
-        
-    
+
+
 }
 
 
