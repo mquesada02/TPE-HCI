ed4ed4c225b77e1d8d361932aa632afd5910a0e3
mquesada02 <mquesada@itba.edu.ar>
2023-10-22 17:29:49 -0300

    Favourites

--- fiit-web/src/views/FavoritesView.vue
+++ fiit-web/src/views/FavoritesView.vue
@@ -4,7 +4,7 @@
       <v-container>
         <FavsRec title="Recientes" :imgs="recientes"/>
         <h1 class="pt-5">Mis favoritos:</h1>
-        <Myfavs :items="recientes2" :text="texto"/>
+        <Myfavs :items="myfavs" :text="texto"/>
       </v-container>
     </v-main>
   </v-app>
@@ -13,13 +13,24 @@
 <script setup>
     import FavsRec from '@/components/Recently.vue';
     import Myfavs from '@/components/RoutineIter.vue';
+    import { useRoutineStore } from '@/stores/routineStore';
+    import { onBeforeMount, ref } from 'vue';
     const texto = "No has agregado ninguna rutina a favoritos"
+
+    const myfavs = ref([])
+
+    onBeforeMount(async () => {
+      const routineStore = useRoutineStore();
+      const favourites = await routineStore.retrieveFavourites();
+        favourites.forEach((elem) => {
+          myfavs.value.push({src: elem.metadata.img, title: elem.name, id: elem.id})
+        })
+    })
+
     const recientes = [
     { src: 'abdominales bicicleta.webp', title: 'Abdominales en bicicleta' },
     { src: 'abductores.jpg', title: 'Abductores' },
     { src: 'dominadas.jpg', title: 'Dominadas' },
     { src: 'elevaciones laterales.jpeg', title: 'Elevaciones laterales' }
   ];
-  const recientes2 = [
-    ];
 </script>
