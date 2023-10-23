b95fc389334aafcfe19ee3f0d6de994f9e96f571
Thomas Ruijgt <truijgt@itba.edu.ar>
2023-10-23 00:37:46 -0300

    NotLogIn en search

--- fiit-web/src/views/SearchView.vue
+++ fiit-web/src/views/SearchView.vue
@@ -1,10 +1,15 @@
 <template>
-    <v-app id="searchScreen">
+    <div v-if="userStore.isLoggedIn">
+        <v-app id="searchScreen">
         <SearchBar />
         <v-container>
         <MyRout :items="myroutines" :text="texto"/>
     </v-container>
     </v-app>
+    </div>
+    <div v-else>
+        <NotLogIn/>
+    </div>
 </template>
 
 <style scoped>
@@ -16,10 +21,11 @@
 <script setup>
     import SearchBar from '@/components/SearchBar.vue';
     import MyRout from '@/components/RoutineIter.vue';
+    import NotLogIn from '@/views/NotLogInView.vue';
     import { onBeforeMount, ref } from 'vue';
     import { useRoutineStore } from '@/stores/routineStore';
-import { inject } from 'vue';
-
+    import { useUserStore } from '@/stores/userStore';
+    const userStore = useUserStore();
     const texto="No se encontraron resultados para tu búsqueda"
     const myroutines = ref([]);
     const query = '';
@@ -31,7 +37,6 @@ import { inject } from 'vue';
     routines.value.content.forEach((elem) => {
       myroutines.value.push({src: elem.metadata.img, title: elem.name, id: elem.id})
     })
-    
     while (!routines.value.isLastPage) {
       routines.value = await routineStore.filterRoutinesByPage(++i, query);
       routines.value.content.forEach((elem) => {
