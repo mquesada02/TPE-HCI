71a0fa131b2d378650330e8f92cb8a3bbb94232d
unknown <camuller@itba.edu.ar>
2023-10-24 14:39:57 -0300

    vista de ejercicio y modificar ejercicio

--- fiit-web/src/components/ExcersiveCard.vue
+++ fiit-web/src/components/ExcersiveCard.vue
@@ -1,10 +1,11 @@
 <template>
-    <v-card color="secondary" class="ma-4"
+    <v-card :rounded="0" color="lighter" class="ma-4"
             width="200"
+            height="200"
             @:click="toggle"
             :to="link"
             >
-        <v-card height="75%">
+        <v-card :rounded="0" height="75%">
             <v-img :src="img" cover/>   
         </v-card>
         <v-card-text class="pa-2 font-weight-medium text-center" height="25%"> {{ cardTitle }}</v-card-text>
