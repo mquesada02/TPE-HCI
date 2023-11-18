63a17ad19185883437ab93a5c44ee16cfbd5c913
mquesada02 <mquesada@itba.edu.ar>
2023-11-18 11:23:59 -0300

    Execute Routine

--- fiit-mobile/app/src/main/java/ar/edu/itba/hci/fiit_mobile/Components/MenuCards.kt
+++ fiit-mobile/app/src/main/java/ar/edu/itba/hci/fiit_mobile/Components/MenuCards.kt
@@ -1,19 +1,15 @@
 package ar.edu.itba.hci.fiit_mobile.Components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -22,13 +18,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import ar.edu.itba.hci.fiit_mobile.R
 
 @Composable
-//@Preview(showBackground = true, name = "Light Mode", uiMode = Configuration.UI_MODE_NIGHT_NO)
 fun MenuCard(name: String, iconType : ImageVector) {
 
     Card(
