ab9539fade74ce431328afd3b0f71b6bb7c85970
catamuller <camuller@itba.edu.ar>
2023-11-22 14:49:17 -0300

    order by :D

--- fiit-mobile/app/src/main/java/ar/edu/itba/hci/fiit_mobile/Components/RoutineCard.kt
+++ fiit-mobile/app/src/main/java/ar/edu/itba/hci/fiit_mobile/Components/RoutineCard.kt
@@ -1,6 +1,5 @@
 package ar.edu.itba.hci.fiit_mobile.Components
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -8,39 +7,29 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material3.Card
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
-import ar.edu.itba.hci.fiit_mobile.Screen
 import ar.edu.itba.hci.fiit_mobile.R
+import ar.edu.itba.hci.fiit_mobile.Screen
 import ar.edu.itba.hci.fiit_mobile.WindowInfo
 import ar.edu.itba.hci.fiit_mobile.data.network.model.routines.NetworkRoutineContent
-import ar.edu.itba.hci.fiit_mobile.data.network.model.routines.NetworkRoutineMetadata
-import ar.edu.itba.hci.fiit_mobile.data.network.model.user.NetworkUser
 import ar.edu.itba.hci.fiit_mobile.rememberWindowInfo
 import ar.edu.itba.hci.fiit_mobile.ui.viewmodels.HomeViewModel
 import ar.edu.itba.hci.fiit_mobile.util.getViewModelFactory
@@ -67,8 +56,8 @@ fun RoutineCard(data : NetworkRoutineContent, viewModel: HomeViewModel = viewMod
                     navController.navigate(Screen.RoutinesScreen.route)
                 })
                 .aspectRatio(1f)
-                .background(MaterialTheme.colorScheme.background)
                 .padding(12.dp),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondary)
 
             ) {
             Column(
--- fiit-mobile/app/src/main/java/ar/edu/itba/hci/fiit_mobile/ui/views/SearchView.kt
+++ fiit-mobile/app/src/main/java/ar/edu/itba/hci/fiit_mobile/ui/views/SearchView.kt
@@ -1,21 +1,21 @@
 package ar.edu.itba.hci.fiit_mobile.ui.views
 
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Box
-import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,7 +28,9 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import ar.edu.itba.hci.fiit_mobile.Components.RoutineScroller
 import ar.edu.itba.hci.fiit_mobile.R
+import ar.edu.itba.hci.fiit_mobile.WindowInfo
 import ar.edu.itba.hci.fiit_mobile.data.network.model.routines.NetworkRoutineContent
+import ar.edu.itba.hci.fiit_mobile.rememberWindowInfo
 import ar.edu.itba.hci.fiit_mobile.ui.viewmodels.SearchViewModel
 import ar.edu.itba.hci.fiit_mobile.util.getViewModelFactory
 
@@ -57,71 +59,176 @@ fun SearchScreen(
         mutableStateOf("")
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        TextField(
-            value = searchText,
-            onValueChange = viewModel::onSearchTextChange,
-            modifier = Modifier.fillMaxWidth(),
-            placeholder = {Text(stringResource(R.string.search))}
-        )
+    val date = stringResource(R.string.ddmenu_date)
+    val intensity = stringResource(R.string.ddmenu_intensity)
+    val score = stringResource(R.string.ddmenu_score)
+    val routinename = stringResource(R.string.ddmenu_routinename)
+    val username = stringResource(R.string.ddmenu_username)
 
-        ExposedDropdownMenuBox(
-            expanded = isExpanded,
-            onExpandedChange = {isExpanded = it},
-            modifier = Modifier.padding(10.dp)
+    val windowInfo = rememberWindowInfo()
+
+    if(windowInfo.screenWidthInfo is WindowInfo.WindowType.Compact) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             TextField(
-                value = orderBy,
-                onValueChange = {},
-                readOnly = true,
-                trailingIcon = {
-                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
-                },
-                colors = ExposedDropdownMenuDefaults.textFieldColors(),
-                modifier = Modifier.menuAnchor()
+                value = searchText,
+                onValueChange = viewModel::onSearchTextChange,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                placeholder = { Text(stringResource(R.string.search)) },
+                colors = TextFieldDefaults.textFieldColors(containerColor= MaterialTheme.colorScheme.primary)
             )
-            ExposedDropdownMenu(
-                expanded = isExpanded, onDismissRequest = { isExpanded = false }
+
+            ExposedDropdownMenuBox(
+                expanded = isExpanded,
+                onExpandedChange = { isExpanded = it },
+                modifier = Modifier
+                    .padding(6.dp)
+                    .fillMaxWidth(0.8f),
             ) {
-                DropdownMenuItem(
-                    text = { stringResource(R.string.ddmenu_date) },
-                    onClick = { orderBy = "date" }
+                TextField(
+                    value = orderBy,
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
+                    },
+                    placeholder = {
+                        Text(stringResource(R.string.order_by))
+                    },
+                    colors = TextFieldDefaults.textFieldColors(containerColor= MaterialTheme.colorScheme.primary),
+                    modifier = Modifier.menuAnchor()
                 )
-                DropdownMenuItem(
-                    text = { stringResource(R.string.ddmenu_intensity) },
-                    onClick = { orderBy = "intensity" }
-                )
-                DropdownMenuItem(
-                    text = { stringResource(R.string.ddmenu_score) },
-                    onClick = { orderBy = "score" }
-                )
-                DropdownMenuItem(
-                    text = { stringResource(R.string.ddmenu_routinename) },
-                    onClick = { orderBy = "routine" }
-                )
-                DropdownMenuItem(
-                    text = { stringResource(R.string.ddmenu_username) },
-                    onClick = { orderBy = "user" }
+                ExposedDropdownMenu(
+                    expanded = isExpanded, onDismissRequest = { isExpanded = false }
+                ) {
+                    DropdownMenuItem(
+                        text = { Text(text = date) },
+                        onClick = { orderBy=date; viewModel.orderRoutines("date"); isExpanded = false}
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = intensity) },
+                        onClick = { orderBy = intensity; viewModel.orderRoutines("difficulty"); isExpanded = false }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = score) },
+                        onClick = { orderBy = score; viewModel.orderRoutines("score"); isExpanded = false }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = routinename) },
+                        onClick = { orderBy = routinename; viewModel.orderRoutines("routinename"); isExpanded = false }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = username) },
+                        onClick = { orderBy = username; viewModel.orderRoutines("user"); isExpanded = false }
+                    )
+                }
+            }
+
+            if (uiState.isLoading) {
+                if (routines.isNotEmpty()) {
+                    viewModel.updateLoad()
+                }
+                Box(modifier = Modifier.fillMaxSize()) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+            } else {
+                RoutineScroller(
+                    name = null,
+                    routines = routines as ArrayList<NetworkRoutineContent>
                 )
             }
         }
+    } else { //-------otro tamaño----------
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                TextField(
+                    value = searchText,
+                    onValueChange = viewModel::onSearchTextChange,
+                    modifier = Modifier
+                        .fillMaxWidth(0.7f)
+                        .padding(6.dp),
+                    placeholder = { Text(stringResource(R.string.search)) },
+                    colors = TextFieldDefaults.textFieldColors(containerColor= MaterialTheme.colorScheme.primary)
+                )
 
-        if(uiState.isLoading){
-            if (routines.isNotEmpty()) {
-                viewModel.updateLoad()
+                ExposedDropdownMenuBox(
+                    expanded = isExpanded,
+                    onExpandedChange = { isExpanded = it },
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .fillMaxWidth(),
+                ) {
+                    TextField(
+                        value = orderBy,
+                        onValueChange = {},
+                        readOnly = true,
+                        trailingIcon = {
+                            ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
+                        },
+                        placeholder = {
+                            Text(stringResource(R.string.order_by))
+                        },
+                        colors = TextFieldDefaults.textFieldColors(containerColor= MaterialTheme.colorScheme.primary),
+                        modifier = Modifier.menuAnchor()
+                    )
+                    ExposedDropdownMenu(
+                        expanded = isExpanded, onDismissRequest = { isExpanded = false }
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text(text = date) },
+                            onClick = { orderBy=date; viewModel.orderRoutines("date"); isExpanded = false}
+                        )
+                        DropdownMenuItem(
+                            text = { Text(text = intensity) },
+                            onClick = { orderBy = intensity; viewModel.orderRoutines("difficulty"); isExpanded = false }
+                        )
+                        DropdownMenuItem(
+                            text = { Text(text = score) },
+                            onClick = { orderBy = score; viewModel.orderRoutines("score"); isExpanded = false }
+                        )
+                        DropdownMenuItem(
+                            text = { Text(text = routinename) },
+                            onClick = { orderBy = routinename; viewModel.orderRoutines("routinename"); isExpanded = false }
+                        )
+                        DropdownMenuItem(
+                            text = { Text(text = username) },
+                            onClick = { orderBy = username; viewModel.orderRoutines("user"); isExpanded = false }
+                        )
+                    }
+                }
             }
-            Box(modifier = Modifier.fillMaxSize()){
-                CircularProgressIndicator(
-                    modifier = Modifier.align(Alignment.Center)
+
+
+            if (uiState.isLoading) {
+                if (routines.isNotEmpty()) {
+                    viewModel.updateLoad()
+                }
+                Box(modifier = Modifier.fillMaxSize()) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+            } else {
+                RoutineScroller(
+                    name = null,
+                    routines = routines as ArrayList<NetworkRoutineContent>
                 )
             }
-        } else {
-            RoutineScroller(name = null, routines = routines as ArrayList<NetworkRoutineContent>)
         }
     }
 
